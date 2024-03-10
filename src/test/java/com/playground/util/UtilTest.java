50cb367e6cfeec08974e352d7a7ebc3a345baee6
Bruce Shen <blueaken@gmail.com>
2024-03-10 22:06:20 +0800

    init

---
+++ src/test/java/com/playground/util/UtilTest.java
@@ -0,0 +1,13 @@
+package com.playground.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UtilTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
